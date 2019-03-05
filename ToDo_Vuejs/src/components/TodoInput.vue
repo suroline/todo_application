5b42bd083cf47083e9e5229bbb07bbb8190000cc
suroline <suroline@gmail.com>
2019-03-05 17:40:16 +0900

    Vue.js study

---
+++ ToDo_Vuejs/src/components/TodoInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="inputBox shadow">
+    <input
+      v-model="newTodoItem"
+      type="text"
+      placeholder="Type what you have to do"
+      @keyup.enter="addTodo">
+    <span
+      class="addContainer"
+      @click="addTodo">
+      <i
+        class="addBtn fas fa-plus"
+        aria-hidden="ture"/>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo () {
+      if (this.newTodoItem !== '') {
+        var value = this.newTodoItem && this.newTodoItem.trim()
+        // localStorage.setItem(value, value)
+        this.$emit('addTodo', value)
+        this.clearInput()
+      }
+    },
+    clearInput () {
+      this.newTodoItem = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background-color: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
+</style>
---
+++ ToDo_Vuejs/vue.js_study_diary.md
@@ -0,0 +1,13 @@
+20190216
+
+- Disctionary
+
+1. Framework: tool that helps developers build service effeciently (which is an essential supporting structure of a building)
+2. Routing: the process of selecting a path for a traffic in network, or between or across the multiple network
+3. SPA: Single Page Aplliaction
+
+- What is vue.js
+
+1. Front-end framework building user inerfaces
+2. Progressive framework
+3. Function: Router, 
